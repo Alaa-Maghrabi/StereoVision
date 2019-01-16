8a016e4d3bf28070c8359f6b45bcd97fb9e18ad6
alaa.maghrabi <alaa.maghrabi.m@gmail.com>
2019-01-16 11:47:00 +0900

    Updating Repo by adding Realsense files

--- Realsense.py
+++ Realsense.py
@@ -5,7 +5,7 @@ import time
 import orbslam2
 from scipy.signal import butter, lfilter, filtfilt
 import pyrealsense2 as rs
-
+import math
 
 class Realsense:
 
@@ -19,9 +19,10 @@ class Realsense:
         self.f = []
         self.pipeline = []
         self.point_3d = []
+        self.align = []
         # Filter requirements.
         self.order = 3
-        self.fs = 30.0  # sample rate, Hz
+        self.fs = 60.0  # sample rate, Hz
         self.cutoff = 2.  # desired cutoff frequency of the filter, Hz
 
     def butter_lowpass(self):
@@ -40,15 +41,18 @@ class Realsense:
         # Configure depth and color streams
         self.pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.depth, self.w, self.h, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, self.w, self.h, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, self.w, self.h, rs.format.z16, 60)
+        config.enable_stream(rs.stream.color, self.w, self.h, rs.format.bgr8, 60)
 
         # Start streaming
         self.pipeline.start(config)
 
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
+
         if slam_bool:
             vocab_path = "Parameters/ORBvoc.txt"
-            settings_path = "Parameters/EuRoC.yaml"
+            settings_path = "Parameters/Realsense.yaml"
             self.slam = orbslam2.System(vocab_path, settings_path, orbslam2.Sensor.RGBD)
             self.slam.set_use_viewer(True)
             self.slam.initialize()
@@ -93,18 +97,24 @@ class Realsense:
                 yc = int(y)
 
         if show:
-            cv.imshow('Image', image)
+            cv.imshow('Image_', mask)
 
         return xc, yc, int(radius)
 
-    def transform_disp_3d(self, xc, yc, depth_frame, depth_intrin, start):
+    def transform_disp_3d(self, xc, yc, depth_frame, depth_intrin):
         depth = depth_frame.get_distance(xc, yc)
         self.point_3d = rs.rs2_deproject_pixel_to_point(depth_intrin, [xc, yc], depth)
 
     def collect_single_frame_data(self, frames, start, show=False, file_capture=False):
         # This function gets the 3d coordinates of the basketball for one frame, to be called inside a loop
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+        # depth_frame = frames.get_depth_frame()
+        # color_frame = frames.get_color_frame()
+
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
+        # Get aligned frames
+        depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.first(rs.stream.color)
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
@@ -118,7 +128,7 @@ class Realsense:
         xc, yc, _ = self.detect_ball(color_image, show)
 
         # Transform these 2d coordinates into 3d
-        self.transform_disp_3d(xc, yc, depth_frame, depth_intrin, start)
+        self.transform_disp_3d(xc, yc, depth_frame, depth_intrin)
 
         # if we want to save the values in a file
         if file_capture:
@@ -129,9 +139,39 @@ class Realsense:
         if self.point_3d:
             self.out.append([self.point_3d[0], self.point_3d[1], self.point_3d[2]])
 
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
-        cv.imshow('RealSense',images)
+        if show:
+            # Stack both images horizontally
+            images = np.hstack((color_image, depth_colormap))
+            cv.imshow('RealSense',images)
+
+    def collect_frames_data(self, num_frames, show=False, file_capture=False):
+        # This part of the code give you the matrix of 3d coordinates of the ball for X frames
+
+        # Start by initializing the mapping and disparity
+        self.Initialize_Realsense()
+
+        # Start a counter to measure fps
+        start = time.time()
+
+        for frames in range(num_frames):
+            # Wait for a coherent pair of frames: depth and color
+            frames = self.pipeline.wait_for_frames()
+            self.collect_single_frame_data(frames, start, show, file_capture)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # End time
+        end = time.time()
+
+        # Time elapsed
+        seconds = end - start
+        print("Time taken : {0} seconds".format(seconds))
+
+        # Calculate frames per second
+        fps = num_frames / seconds
+        print("Estimated frames per second : {0}".format(fps))
+
+        return self.out
 
     def plot_charts(self):
         # Funtion to plot the 3d coordinates
@@ -163,36 +203,6 @@ class Realsense:
 
         cv.destroyAllWindows()
         self.pipeline.stop()
-
-    def collect_frames_data(self, num_frames, show=False, file_capture=False,
-                            fisheye=True):
-        # This part of the code give you the matrix of 3d coordinates of the ball for X frames
-
-        # Start by initializing the mapping and disparity
-        self.Initialize_Realsense()
-
-        # Start a counter to measure fps
-        start = time.time()
-
-        for frames in range(num_frames):
-            # Wait for a coherent pair of frames: depth and color
-            frames = self.pipeline.wait_for_frames()
-            self.collect_single_frame_data(frames, start, show, file_capture)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # End time
-        end = time.time()
-
-        # Time elapsed
-        seconds = end - start
-        print("Time taken : {0} seconds".format(seconds))
-
-        # Calculate frames per second
-        fps = num_frames / seconds;
-        print("Estimated frames per second : {0}".format(fps))
-
-        return self.out
 
     def save_trajectory(self, filename):
         with open(filename, 'w') as traj_file:
@@ -237,21 +247,26 @@ class Realsense:
         seconds = t - start
         tframe = seconds
 
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
+        # Get aligned frames
+        depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.first(rs.stream.color)
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
+        # Intrinsics & Extrinsics
         depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
-        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
-        depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(color_frame.profile)
-        print('\n \n Depth',depth_intrin)
-        print('\n \n Color',color_intrin)
-        print('\n \n Both', depth_to_color_extrin)
 
         xc, yc, radius = self.detect_ball(color_image, False)
+
+        # Transform these 2d coordinates into 3d
+        self.transform_disp_3d(xc, yc, depth_frame, depth_intrin)
+
+        if self.point_3d:
+            self.out.append([self.point_3d[0], self.point_3d[1], self.point_3d[2]])
 
         t1 = time.time()
         self.slam.process_image_rgbd(color_image, depth_image, tframe)
@@ -274,27 +289,29 @@ class Realsense:
         print('-----')
         print('Start processing sequence ...')
         ball = []
+        glob_XX = []
+        glob_YY = []
+        glob_ZZ = []
+
         for idx in range(num_frames):
             # Wait for a coherent pair of frames: depth and color
             frames = self.pipeline.wait_for_frames()
             times_track[idx], xc, yc, radius = self.SLAM_single_cycle(frames, start)
-            #self.collect_single_frame_data(frames, start, False, False)
+            current_traj = self.slam.get_trajectory_points()
+            #print(current_traj)
+            out = self.out
+            out = np.array(out)
+            current_traj = np.array(current_traj)
 
-            # traj = self.slam.get_keyframe_XY()
-            # if len(traj) > 0:
-            #     traj = np.array(traj)
-            #     if xc > 0 and yc > 0:
-            #         indexes = np.where((traj[:, 0] > xc - radius / 2) & (traj[:, 0] < xc + radius / 2) &
-            #                            (traj[:, 1] > yc - radius / 2) & (traj[:, 1] < yc + radius / 2))[0]
-            #         # print(traj[:,0], traj[:,1])
-            #         if indexes.any() == True and traj[indexes[0], 4] < 2 and traj[indexes[0], 4] > 0.1:
-            #             # ball.append([traj[indexes[:],3], traj[indexes[:],5]])
-            #             ball.append([np.mean(traj[indexes[:], 2], axis=0),
-            #                          np.mean(traj[indexes[:], 4], axis=0)])
+            glob_XX.append((out[-1, 0]*current_traj[-1, 1]) + (out[-1, 1]*current_traj[-1, 2])
+                          + (out[-1, 2]*current_traj[-1,3]) + current_traj[-1,4])
+            glob_YY.append((out[-1, 0]*current_traj[-1, 5]) + (out[-1, 1]*current_traj[-1, 6])
+                          + (out[-1, 2]*current_traj[-1,7]) + current_traj[-1,8])
+            glob_ZZ.append((out[-1, 0]*current_traj[-1, 9]) + (out[-1, 1]*current_traj[-1, 10])
+                          + (out[-1, 2]*current_traj[-1,11]) + current_traj[-1,12])
 
         # End time
         end = time.time()
-
         # Time elapsed
         seconds = end - start
         print("Time taken : {0} seconds".format(seconds))
@@ -309,17 +326,22 @@ class Realsense:
         trajec = self.slam.get_trajectory_points()
         trajec = np.array(trajec)
 
-        # self.out = np.array(self.out)
+        self.out = np.array(self.out)
 
-        ball = np.array(ball)
-        print(ball)
+        glob_X = np.array(glob_XX)
+        glob_Y = np.array(glob_YY)
+        glob_Z = np.array(glob_ZZ)
+        #glob_X = np.multiply(self.out[:, 0],trajec[:, 1]) + np.multiply(self.out[:, 1],trajec[:, 2]) + np.multiply(self.out[:, 2],trajec[:, 3]) + trajec[:, 4]
+        #glob_Y = np.multiply(self.out[:, 0],trajec[:, 5]) + np.multiply(self.out[:, 1],trajec[:, 6]) + np.multiply(self.out[:, 2],trajec[:, 7]) + trajec[:, 8]
+        #glob_Z = np.multiply(self.out[:, 0],trajec[:, 9]) + np.multiply(self.out[:, 1],trajec[:, 10]) + np.multiply(self.out[:, 2],trajec[:, 11]) + trajec[:, 12]
+
         self.slam.shutdown()
         plt.figure('slam')
-        # plt.plot(ball[:, 0], ball[:, 1], 'r', label='3D global', marker='*')
         plt.plot(trajec[:, 4], trajec[:, 12], 'b', label='Stereo system')
-        # plt.plot(trajec[:, 4] + 10, trajec[:, 12], 'k', label='Stereo system second')
-        # plt.plot(self.out[:, 1] / 1000 + 10, self.out[:, 3] / 1000, 'g', label='3D relative', marker='*')
-        plt.legend(loc='upper left')
+        plt.plot(trajec[:, 4] + 10, trajec[:, 12], 'b', label='Stereo system translated')
+        plt.plot(glob_X + 10, glob_Z, 'g', label='3D global translated', marker='*')
+        plt.plot(self.out[:, 0], self.out[:, 2], 'r', label='3D relative', marker='*')
+        plt.legend(loc=2)
         plt.show()
         times_track = sorted(times_track)
         total_time = sum(times_track)
@@ -334,7 +356,7 @@ if __name__ == '__main__':
     # Here is an example of how to run the code to get the coordinates
     num_frames = 600
     disparity_map = Realsense()
-    out = disparity_map.collect_frames_data(num_frames, show= False, fisheye = True)
-    #disparity_map.SLAM(num_frames)
+    #out = disparity_map.collect_frames_data(num_frames, show=False)
+    disparity_map.SLAM(num_frames)
     disparity_map.destroy_feed()
     disparity_map.plot_charts()
